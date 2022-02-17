eb55dff7f570cba98a89b40bcae76daa3148063f
ipgit2860 <techchinyeung@gmail.com>
2022-02-17 23:17:18 +0800

    os

--- helloworld.go
+++ helloworld.go
@@ -1,9 +1,26 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"log"
+	"os"
 )
 
 func main() {
+	executable, _ := os.Executable()
+	curwd, err := os.Getwd()
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	fmt.Println("Hello World!")
+	fmt.Println(executable)
+
+	fmt.Println("current path: ", curwd)
+
+	species := flag.String("species", "gopher", "the species we are studying")
+	flag.Parse()
+	fmt.Println(*species)
+
 }
